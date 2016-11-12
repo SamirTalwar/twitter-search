1493a785c1ea0e7f8e2c522e55897afa5410012d
Samir Talwar <samir@noodlesandwich.com>
2016-11-12 12:42:01 +0000

    Convert callbacks to promises.

--- server.js
+++ server.js
@@ -11,26 +11,24 @@ const env = name => {
   return value
 }
 
+const denodeify = func => (...args) =>
+  new Promise((resolve, reject) => {
+    func(...args, (error, value) => {
+      if (error) {
+        reject(error)
+      } else {
+        resolve(value)
+      }
+    })
+  })
+
 const twitterConsumerKey = env('TWITTER_CONSUMER_KEY')
 const twitterConsumerSecret = env('TWITTER_CONSUMER_SECRET')
 
-const oa = new oauth.OAuth2(
-  twitterConsumerKey,
-  twitterConsumerSecret,
-  'https://api.twitter.com/',
-  null,
-  'oauth2/token',
-  null)
-oa.getOAuthAccessToken(
-  '',
-  {'grant_type': 'client_credentials'},
-  (error, accessToken, refreshToken, results) => {
-    if (error) {
-      console.error(error)
-      process.exitCode = 1
-      return
-    }
+const oa = new oauth.OAuth2(twitterConsumerKey, twitterConsumerSecret, 'https://api.twitter.com/', null, 'oauth2/token', null)
 
+denodeify(oa.getOAuthAccessToken.bind(oa))('', {'grant_type': 'client_credentials'})
+  .then(accessToken => {
     oa.useAuthorizationHeaderforGET(true)
 
     const endpoint = 'https://api.twitter.com/1.1/search/tweets.json'
@@ -39,13 +37,12 @@ oa.getOAuthAccessToken(
       count: 100
     })
     process.stderr.write('Searching...\n')
-    oa.get(url, accessToken, (error, data) => {
-      if (error) {
-        console.error(error)
-        process.exitCode = 1
-        return
-      }
-
-      console.log(data)
-    })
+    return denodeify(oa.get.bind(oa))(url, accessToken)
+  })
+  .then(data => {
+    console.log(data)
+  })
+  .catch(error => {
+    console.error(error)
+    process.exitCode = 1
   })
