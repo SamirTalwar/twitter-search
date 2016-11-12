757843d415b4cb44d40540ca0e1b46d4161ccd8c
Samir Talwar <samir@noodlesandwich.com>
2016-11-12 12:55:30 +0000

    Move the WebSocket server into the server app.

--- Makefile
+++ Makefile
@@ -1,17 +1,12 @@
 elm := $(shell command -v elm)
 node := $(shell command -v node)
-websocketd := $(shell command -v websocketd)
 
 ifndef elm
-$(error 'This application requires `elm`, `node` and `websocketd`. Please install them before running.')
+$(error 'This application requires `elm` and `node`. Please install them before running.')
 endif
 
 ifndef node
-$(error 'This application requires `elm`, `node` and `websocketd`. Please install them before running.')
-endif
-
-ifndef websocketd
-$(error 'This application requires `elm`, `node` and `websocketd`. Please install them before running.')
+$(error 'This application requires `elm` and `node`. Please install them before running.')
 endif
 
 build: client.js
@@ -20,4 +15,4 @@ client.js: Client.elm
 	$(elm) make --output=$@ $^
 
 run:
-	./server "$$QUERY"
+	./server.js "$$QUERY"
--- server.js
+++ server.js
@@ -2,9 +2,10 @@
 
 const oauth = require('oauth')
 const querystring = require('querystring')
+const WebSocketServer = require('ws').Server
 
-const env = name => {
-  const value = process.env[name]
+const env = (name, defaultValue) => {
+  const value = process.env[name] || defaultValue
   if (!value) {
     throw new Error(`No such environment variable: ${name}`)
   }
@@ -22,25 +23,34 @@ const denodeify = func => (...args) =>
     })
   })
 
+const port = parseInt(env('PORT', '8081'), 10)
+const searchInterval = parseInt(env('SEARCH_INTERVAL', '15000'), 10)
 const twitterConsumerKey = env('TWITTER_CONSUMER_KEY')
 const twitterConsumerSecret = env('TWITTER_CONSUMER_SECRET')
+const twitterSearchUrl = 'https://api.twitter.com/1.1/search/tweets.json?' + querystring.stringify({
+  q: process.argv[2],
+  count: 100
+})
 
 const oa = new oauth.OAuth2(twitterConsumerKey, twitterConsumerSecret, 'https://api.twitter.com/', null, 'oauth2/token', null)
+oa.useAuthorizationHeaderforGET(true)
+const getOAuthAccessToken = denodeify(oa.getOAuthAccessToken.bind(oa))
+const get = denodeify(oa.get.bind(oa))
 
-denodeify(oa.getOAuthAccessToken.bind(oa))('', {'grant_type': 'client_credentials'})
+getOAuthAccessToken('', {'grant_type': 'client_credentials'})
   .then(accessToken => {
-    oa.useAuthorizationHeaderforGET(true)
-
-    const endpoint = 'https://api.twitter.com/1.1/search/tweets.json'
-    const url = endpoint + '?' + querystring.stringify({
-      q: process.argv[2],
-      count: 100
-    })
-    process.stderr.write('Searching...\n')
-    return denodeify(oa.get.bind(oa))(url, accessToken)
-  })
-  .then(data => {
-    console.log(data)
+    const server = new WebSocketServer({port})
+    const search = () => {
+      console.log('Searching...')
+      return get(twitterSearchUrl, accessToken)
+        .then(data => {
+          server.clients.forEach(client => {
+            client.send(data)
+          })
+        })
+        .then(() => setTimeout(search, searchInterval))
+    }
+    return search()
   })
   .catch(error => {
     console.error(error)
