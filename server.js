5e6c741ead84fef6e232a8b693a117d7c2f506f6
Samir Talwar <samir@noodlesandwich.com>
2016-11-12 13:49:05 +0000

    Use the *twitter* package instead of the *oauth* one.

--- server.js
+++ server.js
@@ -2,8 +2,9 @@
 
 const http = require('http')
 const fs = require('fs')
-const oauth = require('oauth')
 const querystring = require('querystring')
+const request = require('request')
+const Twitter = require('twitter')
 const WebSocketServer = require('ws').Server
 
 const env = (name, defaultValue) => {
@@ -16,11 +17,11 @@ const env = (name, defaultValue) => {
 
 const denodeify = func => (...args) =>
   new Promise((resolve, reject) => {
-    func(...args, (error, value) => {
+    func(...args, (error, ...values) => {
       if (error) {
         reject(error)
       } else {
-        resolve(value)
+        resolve(values.length === 1 ? value : values)
       }
     })
   })
@@ -29,17 +30,9 @@ const clientPort = parseInt(env('CLIENT_PORT', '8080'), 10)
 const serverPort = parseInt(env('SERVER_PORT', '8081'), 10)
 
 const searchInterval = parseInt(env('SEARCH_INTERVAL', '15000'), 10)
+const searchQuery = process.argv[2]
 const twitterConsumerKey = env('TWITTER_CONSUMER_KEY')
 const twitterConsumerSecret = env('TWITTER_CONSUMER_SECRET')
-const twitterSearchUrl = 'https://api.twitter.com/1.1/search/tweets.json?' + querystring.stringify({
-  q: process.argv[2],
-  count: 100
-})
-
-const oa = new oauth.OAuth2(twitterConsumerKey, twitterConsumerSecret, 'https://api.twitter.com/', null, 'oauth2/token', null)
-oa.useAuthorizationHeaderforGET(true)
-const getOAuthAccessToken = denodeify(oa.getOAuthAccessToken.bind(oa))
-const get = denodeify(oa.get.bind(oa))
 
 const runClient = () => {
   const clientServer = http.createServer((request, response) => {
@@ -74,30 +67,59 @@ const runClient = () => {
     })
 }
 
-const runServer = accessToken => {
-  const websocketServer = new WebSocketServer({port: serverPort})
-  console.log(`Server application running on http://localhost:${serverPort}.`)
+const authenticateWithTwitter = () => {
+  const twitterCredentials = Buffer.from(`${encodeURIComponent(twitterConsumerKey)}:${encodeURIComponent(twitterConsumerSecret)}`).toString('base64')
+  return new Promise((resolve, reject) => {
+    const req = request.post({
+      url: 'https://api.twitter.com/oauth2/token',
+      headers: {
+        'Authorization': `Basic ${twitterCredentials}`,
+        'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8',
+      },
+    }, (error, response, body) => {
+      if (error) {
+        return reject(error)
+      }
+      resolve(JSON.parse(body).access_token)
+    })
+    req.write('grant_type=client_credentials')
+    req.end()
+  })
+}
 
-  websocketServer.on('connection', client =>
-    get(twitterSearchUrl, accessToken)
-      .then(client.send.bind(client))
-      .catch(console.error));
-
-  const search = () => {
-    console.log(`Searching at ${new Date()}...`)
-    return get(twitterSearchUrl, accessToken)
-      .then(data => {
-        websocketServer.clients.forEach(client => {
-          client.send(data)
-        })
+const runServer = () => {
+  authenticateWithTwitter()
+    .then(bearerToken => {
+      const twitter = new Twitter({
+        consumer_key: twitterConsumerKey,
+        consumer_secret: twitterConsumerSecret,
+        bearer_token: bearerToken,
       })
-      .then(() => setTimeout(search, searchInterval))
-  }
-  return search()
+      const get = denodeify(twitter.get.bind(twitter))
+
+      const websocketServer = new WebSocketServer({port: serverPort})
+      console.log(`Server application running on http://localhost:${serverPort}.`)
+
+      websocketServer.on('connection', client =>
+        get('search/tweets', {q: searchQuery, count: 100})
+          .then(([_, {body: response}]) => client.send(response))
+          .catch(console.error))
+
+      const search = () => {
+        console.log(`Searching at ${new Date()}...`)
+        return get('search/tweets', {q: searchQuery, count: 100})
+          .then(([_, {body: response}]) => {
+            websocketServer.clients.forEach(client => {
+              client.send(response)
+            })
+          })
+          .then(() => setTimeout(search, searchInterval))
+      }
+      return search()
+    })
 }
 
 runClient()
-  .then(() => getOAuthAccessToken('', {'grant_type': 'client_credentials'}))
   .then(runServer)
   .catch(error => {
     console.error(error)
